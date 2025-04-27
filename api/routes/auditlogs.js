bbbb7ac9a0eb74e1d32a5336fc7fa03ac2bb8362
Sezer Seker <sekersezer.09@gmail.com>
2025-04-28 00:00:14 +0300

    routing alt yapısı kuruldu #1

---
+++ api/routes/auditlogs.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get("/", (req,res,next) => {
+    res.json({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        headers: req.headers,
+    });
+});
+
+module.exports = router;

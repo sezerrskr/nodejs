52aa3586b14a85f830efa6ff83f46bd8b009369d
Sezer Seker <sekersezer.09@gmail.com>
2025-04-29 17:47:57 +0300

    Add CI workflow for Node.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 const fs = require("fs");
 
+// /api/hello route'unu manuel olarak tanımla
+router.get('/hello', (req, res) => {
+  res.status(200).send('Hello, world!');
+});
+
+// Diğer route dosyalarını dinamik yükle
 let routes = fs.readdirSync(__dirname);
 
 for(let route of routes){
   if(route.includes(".js") && route != "index.js"){
-    router.use("/"+route.replace(".js",""), require("./"+route))
+    router.use("/"+route.replace(".js",""), require("./"+route));
   }
 }
 
